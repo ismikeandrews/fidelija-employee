6bbeb94d657b7722a13de3925bdfac6e3f452e2e
Michael Andrews <michaelcl98@hotmail.com>
2021-11-01 12:13:56 -0300

    Adding loading screen and timer to qrcode reader

--- src/Pages/Reader/index.js
+++ src/Pages/Reader/index.js
@@ -1,20 +1,23 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { Container, Paper, Typography, Divider, Button } from '@material-ui/core';
 import QrReader from 'react-qr-reader';
-import { AppBar } from '../../Components';
+import { AppBar, Backdrop } from '../../Components';
 import { styles } from './reader.elements'
 
 const Reader = () => {
+    const [isLoading, setIsLoading] = useState(false);
     const qrRef = useRef(null);
     const classes = styles();
 
     const handleScan = (result) => {
+        setIsLoading(true);
         if(result){
             window.location.replace(result);
         }
     }
 
     const handleUploadScan = (result) => {
+        setIsLoading(true)
         if(result){
             window.location.replace(result);
         }
@@ -23,13 +26,14 @@ const Reader = () => {
     return (
         <div>
             <AppBar/>
+            <Backdrop open={isLoading}/>
             <Container>
                 <div className={classes.header}>
                     <Typography variant="h5">Leitor de código QR</Typography>
                     <Divider/>
                 </div>
                 <Paper variant="outlined" className={classes.paperContent}>
-                    <QrReader className={classes.qr} delay={300} onError={(error) => console.log(error)} onScan={handleScan}/>
+                    <QrReader className={classes.qr} delay={1300} onError={(error) => console.log(error)} onScan={handleScan}/>
                 </Paper>
                 <Button fullWidth variant="contained" color="primary" className={classes.button} onClick={() => qrRef.current.openImageDialog()}>Upload do Código QR</Button>
                 <QrReader
